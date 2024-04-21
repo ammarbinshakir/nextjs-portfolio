1449d42ad99d779002bae4e51c4e1916ba8b844f
ammar <ammarbinshakir557@gmail.com>
2024-04-21 17:01:17 +0500

    canonical

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="canonical" href="https://ammar.geekscrew.xyz" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
